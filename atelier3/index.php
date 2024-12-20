fe3d0a9eb919ad65ff1c121f3d9264b338b3a778
Ahoua685 <ahouacool@gmail.com>
2024-12-20 11:16:42 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   else {
         $error = "Nom d'utilisateur ou mot de passe incorrect."; 
   }
- if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true)&& $_SESSION['username']==='user') {
+ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true && $_SESSION['username']==='user') {
     header('Location: page_user.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
 } 
